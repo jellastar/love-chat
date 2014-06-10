4b09f5cf61ae41414f4c0c3f6fa8e02ec4c544c9
vu.cntt91 <vu.cntt91@gmail.com>
2014-06-10 08:48:43 +0000

    LoginActivity.java MainFragment.java activity_login.java AndroidManifest.xml

---  love-chat/LoveChat/src/demo/example/lovechat/LoginActivity.java
+++  love-chat/LoveChat/src/demo/example/lovechat/LoginActivity.java
@@ -1,21 +1,22 @@
 package demo.example.lovechat;
 
-import com.facebook.Session;
-import com.facebook.SessionState;
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.Toast;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-public class LoginActivity extends Activity
+import demo.example.lovechat.MainFragment;
+import android.os.Bundle;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.Menu;
+
+public class LoginActivity extends FragmentActivity
 {
 
-	private Button btnLogin;
-	private Session.StatusCallback statusCallback = new SessionStatusCallback();
+	private MainFragment mainfragment;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -23,67 +24,17 @@ public class LoginActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		
-		btnLogin=(Button)findViewById(R.id.login_button);
-		
-		btnLogin.setOnClickListener(new OnClickListener() {
-            public void onClick(View view) { onClickLogin(); }
-        });
-		// start Facebook Login
-		
-		Session session = Session.getActiveSession();
-        if (session == null) {
-            if (savedInstanceState != null) {
-                session = Session.restoreSession(this, null, statusCallback, savedInstanceState);
-            }
-            if (session == null) {
-                session = new Session(this);
-            }
-            Session.setActiveSession(session);
-            if (session.getState().equals(SessionState.CREATED_TOKEN_LOADED)) {
-                session.openForRead(new Session.OpenRequest(this).setCallback(statusCallback));
-            }
-        }
-        onLoginSuccess();
+		if(savedInstanceState==null){
+			mainfragment= new MainFragment();
+			getSupportFragmentManager().beginTransaction()
+			.add(android.R.id.content, mainfragment).commit();
+		}
+		else {
+			mainfragment =(MainFragment) getSupportFragmentManager()
+					.findFragmentById(android.R.id.content);
+		}
 	}
 	
-	 private void onClickLogin() {
-	        Session session = Session.getActiveSession();
-	        if (!session.isOpened() && !session.isClosed()) {
-	            session.openForRead(new Session.OpenRequest(this).setCallback(statusCallback));
-	        } else {
-	            Session.openActiveSession(this, true, statusCallback);
-	        }
-	    }
-	
-	@Override
-	protected void onStart()
-	{
-		super.onStart();
-        Session.getActiveSession().addCallback(statusCallback);
-	}
-
-	@Override
-	protected void onStop()
-	{
-		super.onStop();
-        Session.getActiveSession().removeCallback(statusCallback);    
-	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data)
-	{
-		super.onActivityResult(requestCode, resultCode, data);
-        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
-    }
-
-	@Override
-	protected void onSaveInstanceState(Bundle outState)
-	{
-		super.onSaveInstanceState(outState);
-        Session session = Session.getActiveSession();
-        Session.saveSession(session, outState);
-    }
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
@@ -91,17 +42,5 @@ public class LoginActivity extends Activity
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
-	
-	private void onLoginSuccess()
-	{
-		Toast.makeText(getApplicationContext(), "Success",Toast.LENGTH_LONG).show();
-	}
-	private class SessionStatusCallback implements Session.StatusCallback {
-        @Override
-        public void call(Session session, SessionState state, Exception exception) {
-        	onLoginSuccess();
-        }
-    }
-	
 
 }
